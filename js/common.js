a62bdc44c330320e195f46424ca4a5ed89233fbf
apospelov13 <small99@hotbox.ru>
2017-07-01 16:12:49 +0300

    v06

--- js/common.js
+++ js/common.js
@@ -1,11 +1,23 @@
 $(document).ready(function(){
-	alert("privet");
+	alert("privet2");
 	$(".btn_menusecond").click(function(){
 		$(".menusecond").toggle("slow");
 	});	
 	$(".btn_brandrepair").click(function(){
 		$(".brandrepair_item-hide").toggle("slow");
 	});	
+	function mediaSize() { 
+		if (window.matchMedia('(min-width: 768px) and (max-width: 1023px)').matches) {
+			$(".brandrepair_item").slice(15,).addClass("brandrepair_item-hide").hide();
+		} else if (window.matchMedia('(max-width: 767px)').matches) {
+			$(".brandrepair_item").slice(9,).addClass("brandrepair_item-hide").hide();
+		}
+		else if (window.matchMedia('(min-width: 1024px)').matches) {
+			$(".brandrepair_item-hide").show();	
+		}
+	};
+  mediaSize();
+	window.addEventListener('resize', mediaSize, false);  
 });
 	
 	
