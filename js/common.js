bd8a80218ee2f8fcb029e2fac483ec571be83a7c
apospelov13 <small99@hotbox.ru>
2017-07-01 16:07:31 +0300

    v05

--- js/common.js
+++ js/common.js
@@ -1,15 +1,11 @@
 $(document).ready(function(){
 	alert("privet");
-	//menusecond collapse
 	$(".btn_menusecond").click(function(){
 		$(".menusecond").toggle("slow");
 	});	
-	//menusecond collapse END
-	//brandrepair collapse on 1024 and 767
 	$(".btn_brandrepair").click(function(){
 		$(".brandrepair_item-hide").toggle("slow");
 	});	
-	//brandrepair collapse on 1024 and 767 END
 });
 	
 	
