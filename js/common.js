ccf8be78cccedd05ed063f4f37a94f7904320e2e
apospelov13 <small99@hotbox.ru>
2017-07-01 16:04:37 +0300

    v04

--- js/common.js
+++ js/common.js
@@ -1,4 +1,5 @@
 $(document).ready(function(){
+	alert("privet");
 	//menusecond collapse
 	$(".btn_menusecond").click(function(){
 		$(".menusecond").toggle("slow");
@@ -9,20 +10,6 @@ $(document).ready(function(){
 		$(".brandrepair_item-hide").toggle("slow");
 	});	
 	//brandrepair collapse on 1024 and 767 END
-	// media query 1024 and 767 for menusecond and brandrepair
-	function mediaSize() { 
-		if (window.matchMedia('(min-width: 768px) and (max-width: 1023px)').matches) {
-			$(".brandrepair_item").slice(15,).addClass("brandrepair_item-hide").hide();
-		} else if (window.matchMedia('(max-width: 767px)').matches) {
-			$(".brandrepair_item").slice(9,).addClass("brandrepair_item-hide").hide();
-		}
-		else if (window.matchMedia('(min-width: 1024px)').matches) {
-			$(".brandrepair_item-hide").show();	
-		}
-	};
-  mediaSize();
-	window.addEventListener('resize', mediaSize, false);  
-	// media query 1024 and 767 for menusecond and brandrepair END
 });
 	
 	
